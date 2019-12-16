2ce4a51a9d63de9e1c824437b00e239033409159
2Zero1 <Good7219@hotmail.com>
2019-12-16 23:34:43 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-384e42b5473ba127176f.js"
+    "url": "webpack-runtime-d9b55a8ac50c2f23a028.js"
   },
   {
     "url": "styles.6709749d0a729b003adb.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f50a992dbf55a60ed05d34b853f70ba8"
+    "revision": "939a9089912641bc1576de5e08ecb1d3"
   },
   {
     "url": "component---src-pages-404-js-6564d205621fd4993a44.js"
